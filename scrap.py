bd80ebe20ab7120c97ee80a8f4843637f99433cb
rianaditro <rianaditro@gmail.com>
2024-04-22 16:29:22 +0700

    hotfix: scrap.py

--- scrap.py
+++ scrap.py
@@ -5,23 +5,23 @@ from bs4 import BeautifulSoup
 class Scraper:
     def __init__(self):
         self.client = httpx.Client()
+        self.cek_tarif = "https://trackpage.bst-ekspres.com/?cek=tarif"
 
-    def get_html(self, url):
-        # payload = {'prom': 'LGD', 'tujuan': 'RIAU', 'brt': '2', 'da': 'MOJOKERTO', 'tuj': 'PEKANBARU', 'le': '10', 'pa': '10', 'ti': '10', 'hitung': ''}
-        payload = {'prom': 'LGD', 'tujuan': 'RIAU', 'brt': '2', 'da': 'MOJOKERTO', 'tuj': 'PEKANBARU', 'hitung': ''}
-        r = self.client.post(url, data=payload)
+
+    def get_html(self, payload):
+        r = self.client.post(self.cek_tarif, data=payload)
         if r.status_code == 200:
             print("Response: OK")
             html = r.text
             return html
         else:
-            print(f"Get {r.status_code} from {url} with data {payload}")
+            print(f"Get {r.status_code}. Please check {payload}")
 
 class Parser:
     def __init__(self, html):
         self.soup = BeautifulSoup(html, "html.parser")
 
-    def cek_ongkir(self):
+    def extract_ongkir(self):
         result = dict()
 
         table_data = self.soup.find("table", "table table-bordered").find_all("td")
@@ -36,16 +36,18 @@ class Parser:
             for i in range(len(label)):
                 result[label[i]] = "None"
             return result
+        
+def main(payload):
+    scraper = Scraper()
+    html = scraper.get_html(payload)
 
+    parser = Parser(html)
+    ongkir = parser.extract_ongkir()
+    return ongkir
 
 
 if __name__ == "__main__":
-    url = "https://trackpage.bst-ekspres.com/?cek=tarif"
+    payload = {'prom': 'LGD', 'tujuan': 'RIAU', 'brt': '2', 'da': 'MOJOKERTO', 'tuj': 'PEKANBARU', 'hitung': ''}
     
-    scraper = Scraper()
-    html = scraper.get_html(url=url)
-
-    parser = Parser(html).cek_ongkir()
-    print(parser)
-
-    
+    ongkir = main(payload)
+    print(ongkir)
